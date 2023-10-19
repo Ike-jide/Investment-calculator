d2db4e0f41c0c0713c5914a0cddc2acb8c0b4e1a
ike-jide <ikejideofor@gmail.com>
2023-10-19 21:44:22 +0100

    first commit

---
+++ src/Componets/UserInput/ExpenseForm.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import styles from "./ExpenseForm.module.css"
+
+export default function ExpenseForm () {
+    return(
+        <form className={styles.form}>
+        <div className={styles.inputGroup}>
+          <p>
+            <label htmlFor="current-savings">Current Savings ($)</label>
+            <input type="number" id="current-savings" />
+          </p>
+          <p>
+            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+            <input type="number" id="yearly-contribution" />
+          </p>
+        </div>
+        <div className={styles.inputGroup}>
+          <p>
+            <label htmlFor="expected-return">
+              Expected Interest (%, per year)
+            </label>
+            <input type="number" id="expected-return" />
+          </p>
+          <p>
+            <label htmlFor="duration">Investment Duration (years)</label>
+            <input type="number" id="duration" />
+          </p>
+        </div>
+        <p className={styles.actions}>
+          <button type="reset" className={styles.buttonAlt}>
+            Reset
+          </button>
+          <button type="submit" className={styles.button}>
+            Calculate
+          </button>
+        </p>
+      </form>
+    )
+}
